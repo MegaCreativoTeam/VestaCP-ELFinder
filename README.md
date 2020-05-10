80c89ca618a13a10d98d30073e8dc2fae8ab0b1b
Brayan Rincon <brayan262@gmail.com>
2020-05-10 11:24:53 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# VELF - VestaCP EL FINDER 👋
+
+VestaCP El Finder - File Manager for VestaCP
+
+## 🤝 Contributing
+
+Contributions, issues and feature requests are welcome.
+Feel free to check [issues page](https://github.com/megacreativo/VestaCP-ELFinder/issues) if you want to contribute.
+
+## 👤 Author
+
+**Brayan Rincón**
+- Github: [@brayan2rincon](https://github.com/brayan2rincon)
+
+## 📝 License
+
+Copyright © 2020
---
+++ install.sh
@@ -0,0 +1,100 @@
+#!/bin/bash
+
+# Official Cloud Panel Automated Installation Script
+# =============================================
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Supported Operating Systems:
+# CentOS 6/7/8 Minimal
+# Ubuntu server 14.04/18.04
+# Debian 7/8
+# 32bit and 64bit
+#
+
+INSTALLER_VERSION="1.0.0"
+CORE_VERSION="1.0.0"
+
+h1() {
+    echo -e "\e[1m\e[34m$1\e[0m"
+}
+
+h2() {
+    echo -e "\n\e[21m\e[94m$1\e[0m"
+}
+
+success() {
+    echo -e "\e[42m\e[4m[Done]\e[0m $1"
+}
+
+failed() {
+    echo -e "\e[41m\e[4m[Fail]\e[0m $1"
+}
+
+
+# Am I root?
+isRoot(){
+    if [ "x$(id -u)" != 'x0' ]; then
+        echo_failed 'Error: this script can only be executed by root'
+        exit 1
+    fi
+}
+
+#--- Display the 'welcome' splash/user warning info..
+welcome(){
+    clear
+    echo -e ""
+    h1 ""
+    h1 "    __     _______ _     _____  "
+    h1 "    \ \   / / ____| |   |  ___| "
+    h1 "     \ \ / /|  _| | |   | |_    "
+    h1 "      \ V / | |___| |___|  _|   "
+    h1 "       \_/  |_____|_____|_|     "
+    echo ""
+    echo "VestaCP El Finder - File Manager"
+    echo "File Manager for VestaCP"
+    echo ""
+    echo "Copyright © 2020. Powered By Mega Creativo <http://megacreativo.com>"
+    echo -e ""
+}
+
+download(){
+    h2 "Downloading VELF, Please wait, the installer will continue after this is complete!"
+
+    cd /usr/local/vesta/web/list
+   
+    tar -xf velf.tar
+    rm -rf velf.tar
+}
+
+
+# Add the link to the panel.html file 
+install(){
+    if grep -q 'velf' /usr/local/vesta/web/templates/admin/panel.html; then
+        echo 'Already there.'
+    else 
+        sed -i '/<div class="l-menu clearfix noselect">/a <div class="l-menu__item <?php if($TAB == "velf" ) echo "l-menu__item--active"; ?>"><a href="/list/velf/" target="_blank"><?=__("File Manager")?></a></div>' /usr/local/vesta/web/templates/admin/panel.html;
+    fi
+
+    echo 'VestaCP El Finder is Active';
+}
+
+septup(){
+    isRoot
+    welcome
+    download
+    install
+}
+
+septup
